74e8987d9cee9d699d4fff1f6952ded6d5b3574d
drche-jpg <drche@mathmissionth.com>
2026-05-14 14:45:08 +0700

    Update app_20.py

--- app_20.py
+++ app_20.py
@@ -53,7 +53,7 @@ def get_master_df():
 # ─────────────────────────────────────────────
 # URL HELPER
 # ─────────────────────────────────────────────
-DEFAULT_APP_URL = "https://rfidpticmockup-qfn4gve2qgohbwaechw7qh.streamlit.app/"
+DEFAULT_APP_URL = "https://rfidpticmockup-qfn4gve2qgohbwaechw7qh.streamlit.app"
 
 def get_base_url():
     if st.session_state.get("base_url"):
